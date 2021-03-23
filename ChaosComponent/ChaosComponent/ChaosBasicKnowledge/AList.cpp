28d479a264a0492a24e3086731d6aeb335617138
chaizhiyong <703501246@qq.com>
2021-03-23 18:18:51 +0800

    list

--- ChaosComponent/ChaosComponent/ChaosBasicKnowledge/AList.cpp
+++ ChaosComponent/ChaosComponent/ChaosBasicKnowledge/AList.cpp
@@ -90,4 +90,53 @@ template<typename T> Node<T> linkedList<T>:: node(int index){
     return NULL;
 }
 
+template<typename T> int linkedList<T>::indexOf(T element) {
+    if (element == NULL) {
+        Node<T> node = first;
+        for (int i = 0; i < this->size; i++) {
+            if (node.element == NULL) return i;
+            node = node.next;
+        }
+    } else {
+        Node<T> node = first;
+        for (int i = 0; i < this->size; i++) {
+            if (element == node.element) return i;
+            node = node.next;
+        }
+    }
+    return ELEMENT_NOT_FOUND;
+}
 
+template<typename T> Node<T>linkedList<T>:: listNode(int index) {
+   this.rangeCheck(index);
+   if (index < (this->size >> 1)) {
+       Node<T> node = first;
+       for (int i = 0; i < index; i++) {
+           node = node.next;
+       }
+       return node;
+   } else {
+       Node<T> node = last;
+       for (int i = this->size - 1; i > index; i--) {
+           node = node.prev;
+       }
+       return node;
+   }
+}
+
+template<typename T> string linkedList<T>:: toString() {
+        string str1 = "";
+        str1.append("size=").append(this->size).append(", [");
+        Node<T> node = first;
+        for (int i = 0; i < this->size; i++) {
+            if (i != 0) {
+                str1.append(", ");
+            }
+            
+            str1.append(node);
+            
+            node = node.next;
+        }
+        str1.append("]");
+        return str1;
+    }
--- ChaosComponent/ChaosComponent/ChaosBasicKnowledge/AList.hpp
+++ ChaosComponent/ChaosComponent/ChaosBasicKnowledge/AList.hpp
@@ -36,45 +36,10 @@ public:
     T set(int index,T element);
     void add(int index, T element);
     T remove(int index);
+    int indexOf(T element);
+    string toString();
 private:
     Node<T> node(int index);
-public:
-    
-    int indexOf(T element) {
-        if (element == NULL) {
-            Node<T> node = first;
-            for (int i = 0; i < this->size; i++) {
-                if (node.element == NULL) return i;
-                
-                node = node.next;
-            }
-        } else {
-            Node<T> node = first;
-            for (int i = 0; i < this->size; i++) {
-                if (element.equals(node.element)) return i;
-                node = node.next;
-            }
-        }
-        return ELEMENT_NOT_FOUND;
-    }
-    
-private:
-    
-Node<T> listNode(int index) {
-       this.rangeCheck(index);
-       if (index < (this->size >> 1)) {
-           Node<T> node = first;
-           for (int i = 0; i < index; i++) {
-               node = node.next;
-           }
-           return node;
-       } else {
-           Node<T> node = last;
-           for (int i = this->size - 1; i > index; i--) {
-               node = node.prev;
-           }
-           return node;
-       }
-   }
+    Node<T> listNode(int index);
 };
 #endif /* AList_hpp */
