1e7ac889fd29d8af7c69cebb55a76bdfd881f97e
柴志勇 <jinju@7lk.com>
2021-04-17 09:44:58 +0800

    update

--- ChaosComponent/ChaosComponent/ChaosBasicKnowledge/BinaryTree/SearchTree/BTNode.cpp
+++ ChaosComponent/ChaosComponent/ChaosBasicKnowledge/BinaryTree/SearchTree/BTNode.cpp
@@ -6,26 +6,3 @@
 //
 
 #include "BTNode.hpp"
-
-template<typename T> BTNode<T>::BTNode(){}
- 
-template<typename T> BTNode<T>::BTNode(T element, BTNode<T>* parent){
-   this->element = element;
-   this->_parent = parent;
-}
- 
-template<typename T> bool BTNode<T>::isLeaf(){
-    return _left == nullptr && _right == nullptr;
-}
-
-template<typename T> bool BTNode<T>::hasTwoChildren(){
-    return _left != nullptr && _right != nullptr;
-}
-
-template<typename T> bool BTNode<T>::isLeftChild() {
-    return _parent != nullptr && this == _parent->_left;
-}
-
-template<typename T> bool BTNode<T>::isRightChild() {
-     return _parent != nullptr && this == _parent->_right;
-}
--- ChaosComponent/ChaosComponent/ChaosBasicKnowledge/BinaryTree/SearchTree/BTNode.hpp
+++ ChaosComponent/ChaosComponent/ChaosBasicKnowledge/BinaryTree/SearchTree/BTNode.hpp
@@ -17,18 +17,40 @@ public:
     BTNode<T>* _left;
     BTNode<T>* _right;
     BTNode<T>* _parent;
-    BTNode();
-    BTNode(T element, BTNode<T>* parent);
-    
-//    BTNode(){}
-//    BTNode(T element, BTNode<T>* parent){
-//       this->element = element;
-//       this->_parent = parent;
-//    }
-    bool isLeaf();
-    bool hasTwoChildren();
-    bool isLeftChild();
-    bool isRightChild();
+    BTNode(){
+        this->element = NULL;
+        this->_left = nullptr;
+        this->_right = nullptr;
+        this->_parent = nullptr;
+    }
+    BTNode(T element){
+        this->element = element;
+        this->_left = nullptr;
+        this->_right = nullptr;
+        this->_parent = nullptr;
+    }
+    BTNode(T element, BTNode<T>* parent){
+       this->element = element;
+       this->_left = nullptr;
+       this->_right = nullptr;
+       this->_parent = parent;
+    }
+     
+    bool isLeaf(){
+        return _left == nullptr && _right == nullptr;
+    }
+
+    bool  hasTwoChildren(){
+        return _left != nullptr && _right != nullptr;
+    }
+
+    bool  isLeftChild() {
+        return _parent != nullptr && this == _parent->_left;
+    }
+
+    bool isRightChild() {
+         return _parent != nullptr && this == _parent->_right;
+    }
 };
 
 #endif /* BTNode_hpp */
