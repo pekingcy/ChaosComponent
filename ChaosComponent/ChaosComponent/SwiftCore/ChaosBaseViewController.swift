0f9086ac2a6c78d8145becb73909ab2aad7bd694
chaizhiyong <703501246@qq.com>
2021-08-03 19:31:38 +0800

    update

--- ChaosComponent/ChaosComponent/SwiftCore/ChaosBaseViewController.swift
+++ ChaosComponent/ChaosComponent/SwiftCore/ChaosBaseViewController.swift
@@ -9,7 +9,34 @@ import UIKit
 
 class ChaosBaseViewController: UIViewController {
 
+    enum Rank: Int {
+        case ace = 1
+        case two, three, four, five, six, seven, eight, nine, ten
+        case jack, queen, king
+        func simpleDescription() -> String {
+            switch self {
+            case .ace:
+                return "ace"
+            case .jack:
+                return "jack"
+            case .queen:
+                return "queen"
+            case .king:
+                return "king"
+            default:
+                return String(self.rawValue)
+            }
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        if let convertedRank = Rank(rawValue: 3) {
+            let threeDescription = convertedRank.simpleDescription()
+            print(threeDescription)  // [3,4,5,6]
+        }
+       
+      
     }
+    
 }
